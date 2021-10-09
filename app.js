4145de172dd5ca4f6a855bfd32fea51102f45abd
Allan Lavell <alavell@gmail.com>
2021-10-09 20:11:02 -0300

    add cycle library

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-console.log("sup")
+import xs from 'xstream';
+import {run} from '@cycle/run';
+import {makeDOMDriver} from '@cycle/dom';
+import Snabbdom from 'snabbdom-pragma';
 
-document.body.style.backgroundColor = "pink";
+function main(sources) {
+  const dom = {
+		DOM: xs.of(
+			<div>
+				<h2>this is a live prototyping env with cycle.js integration</h2>
+			</div>
+		)
+  }
+
+  return dom;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers);
