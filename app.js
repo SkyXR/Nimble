3be91aef42937eec77d009fb0aebfa6b0fb3c06b
Allan Lavell <alavell@gmail.com>
2021-10-09 21:30:33 -0300

    edit the lil landing page

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ function main(sources) {
 			<div className="mt-10 mb-2 mx-10 text-center">
 				<h1 className="bg-purple-300 mb-5 font-bold text-2xl">this is a live prototyping env with budo, cycle.js, jsx & tailwind</h1>
 				<p className="mb-1">it's a simple way of approaching coding</p>
-				<p className="mb-3">edit js which pulls events from a stream and renders out html in response using frp & jsx</p>
 				<img className="mx-auto w-64" src="castle.jpg" />
+				<p className="mb-3">edit js which pulls events from a stream and renders out html in response using frp & jsx</p>
 			</div>
 		)
   }
