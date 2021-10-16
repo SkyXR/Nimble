a798b12d31a7d0ef73e40347d1b2135e17445b06
Allan Lavell <alavell@gmail.com>
2021-10-16 12:11:31 -0300

    update demo page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function main(sources) {
   const dom = {
 		DOM: xs.of(
 			<div className="mt-10 mb-2 mx-10 text-center">
-				<h1 className="bg-purple-300 mb-5 font-bold text-2xl">this is a live prototyping env with budo, cycle.js, jsx & tailwind</h1>
+				<h1 className="bg-purple-700 mb-5 font-bold text-2xl">this is a live prototyping env with budo, cycle.js, jsx, tailwind & daisyUI</h1>
 				<p className="mb-1">it's a simple way of approaching coding</p>
 				<img className="mx-auto w-64" src="castle.jpg" />
 				<p className="mb-3">edit js which pulls events from a stream and renders out html in response using frp & jsx</p>
